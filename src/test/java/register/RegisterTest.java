737be52e94421b655491e0d74ca9ca46f9f4712f
paula <paulacarneiro@2brain.com.br>
2022-10-22 17:31:51 -0300

    Register

--- src/test/java/register/RegisterTest.java
+++ src/test/java/register/RegisterTest.java
@@ -1,7 +1,31 @@
 package register;
 
+import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import java.time.Duration;
 
 public class RegisterTest {
-WebDriver
+    WebDriver driver;
+
+    @Test
+    public void testRegister() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get("https://demo.automationtesting.in/Register.html");
+        driver.findElement(By.xpath("/html[1]/body[1]/section[1]/div[1]/div[1]/div[2]/form[1]/div[1]/div[1]/input[1]")).sendKeys("Paula");
+        driver.findElement(By.xpath("/html[1]/body[1]/section[1]/div[1]/div[1]/div[2]/form[1]/div[1]/div[2]/input[1]")).sendKeys("Renata");
+        driver.findElement(By.xpath("/html[1]/body[1]/section[1]/div[1]/div[1]/div[2]/form[1]/div[2]/div[1]/textarea[1]")).sendKeys("Almotacel,Brasilandia");
+        // Select
+        Select select;
+        select = new Select(driver.findElement(By.id("Skills")));
+        select.selectByVisibleText("Java");
+
+
+
+    }
 }
